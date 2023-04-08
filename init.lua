870074d9b9160fe74083b2d27b0033b859660a25
Júnior <b.sep@live.com>
2023-04-07 22:47:32 -0300

    usa loop para configurar opções

--- init.lua
+++ init.lua
@@ -1,2 +1,2 @@
-require("junior.options")
+require "junior.options"
 
--- lua/junior/options.lua
+++ lua/junior/options.lua
@@ -1,38 +1,42 @@
-local opt = vim.opt  -- (set vimscript)
+local opt = vim.opt -- (set vimscript)
 
-opt.backup = false                          -- não gera backup
-opt.clipboard = "unnamedplus"               -- da acesso ao clipboard do sistema ao neovim
-opt.cmdheight = 2                           -- espaço na linha de comando do neovim
-opt.completeopt = { "menuone", "noselect" } -- lista de comandos separada por vírgula (insert mode)
-opt.conceallevel = 2                        -- torna `` visivel em arquivos markdown
-opt.ignorecase = true                       -- ignora case em buscas
-opt.mouse = ''                              -- desabilita mouse
-opt.pumheight = 10                          -- número máximo de itens para aparecer em menu popup
-opt.showmode = false                        -- desabilita -- INSERT -- e similares
-opt.showtabline = 2                         -- sempre mostra tabs
-opt.smartcase = true                        -- smart case
-opt.smartindent = true                      -- indentação inteligente
-opt.splitbelow = true                       -- ao fazer split horizontal coloca o novo split embaixo do atual
-opt.splitright = true                       -- ao fazer split vertical coloca o novo split ao lado direito do atual
-opt.swapfile = false                        -- não cria swapfile
+local options = {
+  backup = false,                          -- não gera backup
+  clipboard = "unnamedplus",               -- da acesso ao clipboard do sistema ao neovim
+  cmdheight = 2,                           -- espaço na linha de comando do neovim
+  completeopt = { "menuone", "noselect" }, -- lista de comandos separada por vírgula (insert mode)
+  conceallevel = 2,                        -- torna `` visivel em arquivos markdown
+  ignorecase = true,                       -- ignora case em buscas
+  mouse = '',                              -- desabilita mouse
+  pumheight = 10,                          -- número máximo de itens para aparecer em menu popup
+  showmode = false,                        -- desabilita -- INSERT -- e similares
+  showtabline = 2,                         -- sempre mostra tabs
+  smartcase = true,                        -- smart case
+  smartindent = true,                      -- indentação inteligente
+  splitbelow = true,                       -- ao fazer split horizontal coloca o novo split embaixo do atual
+  splitright = true,                       -- ao fazer split vertical coloca o novo split ao lado direito do atual
+  swapfile = false,                        -- não cria swapfile
+  timeoutlen = 1000,                       -- tempo de espera para combinar comando com várias teclas (default é 1000, deixando aqui para ficar explicito)
+  updatetime = 300,                        -- auto-complete mais rápido (default 4000)
+  writebackup = false,                     -- não gera backup durante a escrita de um arquivo
+  expandtab = true,                        -- converte tab em espaço
+  shiftwidth = 2,                          -- quantidade de espaço inseridos para cada indentação
+  tabstop = 2,                             -- insere 2 espaços a cada tab
+  cursorline = true,                       -- destaca linha atual
+  number = true,                           -- mostra número da linha
+  relativenumber = true,                   -- mostra número relativo a linha atual
+  numberwidth = 4,                         -- tamanho da coluna de números (default é 4, apenas evidenciando)
+  signcolumn = "yes",                      -- sempre mostra coluna de sinalização (antes dos números), caso contrário o texto mudaria
+  wrap = true,                             -- quebra linha caso seja mais que a largura da tela
+  scrolloff = 8,                           -- mínimo de linhas acima e abaixo da linha atual
+  guifont = "monospace:h17",               -- fonte
+  eol = true                               -- adiciona uma linha em branco no final do arquivo
+}
 
-opt.termguicolors = true                    -- define cores do terminal dentro do neovim (desligar depois)
+for k, v in pairs(options) do
+  opt[k] = v
+end
 
-opt.timeoutlen = 1000                       -- tempo de espera para combinar comando com várias teclas (default é 1000, deixando aqui para ficar explicito)
-opt.updatetime = 300                        -- auto-complete mais rápido (default 4000)
-opt.writebackup = false                     -- não gera backup durante a escrita de um arquivo
-opt.expandtab = true                        -- converte tab em espaço
-opt.shiftwidth = 2                          -- quantidade de espaço inseridos para cada indentação
-opt.tabstop = 2                             -- insere 2 espaços a cada tab
-opt.cursorline = true                       -- destaca linha atual
-opt.number = true                           -- mostra número da linha
-opt.relativenumber = true                   -- mostra número relativo a linha atual
-opt.numberwidth = 4                         -- tamanho da coluna de números (default é 4, apenas evidenciando)
-opt.signcolumn = "yes"                      -- sempre mostra coluna de sinalização (antes dos números), caso contrário o texto mudaria
-opt.wrap = true                             -- quebra linha caso seja mais que a largura da tela
-opt.scrolloff = 8                           -- mínimo de linhas acima e abaixo da linha atual
-opt.guifont = "monospace:h17"               -- fonte
-opt.iskeyword:append({'+', '=', '-'})
+opt.iskeyword:append({'+', '=', '-'})      -- trata palavra por +, = ou - como uma única palavra
 
-vim.cmd "set whichwrap+=<,>,[,],h,l"        -- habilita left/right quebrar a linha e subir ou descer de linha quando chegar no final ou começo da mesma
-
+vim.cmd "set whichwrap+=<,>,[,],h,l"       -- habilita left/right quebrar a linha e subir ou descer de linha quando chegar no final ou começo da mesma
