5060ad93441e05f5d0c71d1f52f84f6ee40a98a5
Júnior <b.sep@live.com>
2023-04-18 09:45:57 -0300

    altera configuracao do telescope

--- lua/junior/telescope.lua
+++ lua/junior/telescope.lua
@@ -4,16 +4,14 @@ require("telescope").setup{
   defaults = {
     prompt_prefix = " ",
     selection_caret = " ",
-    path_display = { "smart" },
+    path_display = { "truncate" },
 
     mapping = {
       i = {
         ["<C-c>"] = actions.close,
-        ["<tab>"] = actions.move_selection_next,
-        ["<S-tab>"] = actions.move_selection_previous,
         ["<C-x>"] = actions.select_horizontal,
         ["<C-v>"] = actions.select_vertical,
-     },
+      },
       n = {
         ["<C-x>"] = actions.select_horizontal,
         ["<C-v>"] = actions.select_vertical,
