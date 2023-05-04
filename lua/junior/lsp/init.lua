55e51172287607bbc9b0acd3a930f560533246c8
Roberto <roberto.rodrigues.rebase@youse.com.br>
2023-05-04 09:25:49 -0300

    setup lsp via mason

--- lua/junior/lsp/init.lua
+++ lua/junior/lsp/init.lua
@@ -1,3 +1,2 @@
-require("junior.lsp.lsp_config")
 require("junior.lsp.mason")
 require("junior.lsp.handlers").setup()
--- lua/junior/lsp/mason.lua
+++ lua/junior/lsp/mason.lua
@@ -16,3 +16,22 @@ mason_lsp_config.setup({
   ensure_installed = servers,
   automatic_installation = true
 })
+
+mason_lsp_config.setup_handlers({
+  function(server_name)
+    local opts = {}
+    local handler = require("junior.lsp.handlers")
+
+    opts = {
+      on_attach = handler.on_attach,
+      capabilities = handler.capabilities,
+    }
+
+    local settings_ok, settings = pcall(require, 'junior.lsp.settings.' .. server_name)
+    if settings_ok then
+      opts = vim.tbl_deep_extend('force', settings, opts)
+    end
+
+    require('lspconfig')[server_name].setup(opts)
+  end
+})
