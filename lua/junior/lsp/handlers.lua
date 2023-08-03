153e202d45493e8c37551d887896621fc4c23d3b
Júnior <b.sep@live.com>
2023-08-03 17:51:40 -0300

    torna hover do lsp focavel

--- lua/junior/lsp/handlers.lua
+++ lua/junior/lsp/handlers.lua
@@ -36,7 +36,7 @@ M.setup = function()
 	vim.diagnostic.config(config)
 
 	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-    focusable = false,
+    focusable = true,
 		border = "rounded",
 	})
 
