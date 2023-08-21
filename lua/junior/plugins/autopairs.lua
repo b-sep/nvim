ffdc3ee5cca8339438c6a5665e59bc86fa1ad0fd
Júnior <b.sep@live.com>
2023-08-20 21:21:27 -0300

    fix autopairs

--- lua/junior/plugins/autopairs.lua
+++ lua/junior/plugins/autopairs.lua
@@ -3,13 +3,6 @@ if not status_ok then
   return
 end
 
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local cmp = require('cmp')
-cmp.event:on(
-  'confirm_done',
-  cmp_autopairs.on_confirm_done()
-)
-
 npairs.setup {
   check_ts = true,
 }
