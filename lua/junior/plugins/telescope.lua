fa4f58377d9908b0b9b68b71fd973ab2bc6f41f0
Júnior <b.sep@live.com>
2023-10-29 21:01:44 -0300

    telescope settings

--- lua/junior/plugins/telescope.lua
+++ lua/junior/plugins/telescope.lua
@@ -2,10 +2,19 @@ local actions = require "telescope.actions"
 
 require("telescope").setup{
   defaults = {
+    file_ignore_patterns = { "%.git/.", "node_modules" },
     prompt_prefix = " ",
     selection_caret = " ",
     path_display = { "truncate" },
+    layout_strategy = "center",
+    sorting_strategy = "ascending",
 
+    layout_config = {
+      center = {
+        prompt_position = "top",
+        height = 0.5
+      }
+    },
     mapping = {
       i = {
         ["<C-x>"] = actions.select_horizontal,
@@ -14,12 +23,30 @@ require("telescope").setup{
       n = {
         ["<C-x>"] = actions.select_horizontal,
         ["<C-v>"] = actions.select_vertical,
-     },
+      },
     },
   },
   pickers = {
     find_files = {
-      hidden = true
+      hidden = true,
+      previewer = false
+    },
+    live_grep = {
+      hidden = true,
+      previewer = true,
+      layout_strategy = "horizontal"
+    },
+    current_buffer_fuzzy_find = {
+      theme = "dropdown",
+      previewer = false,
+    },
+    buffers = {
+      theme = "dropdown",
+      previewer = false
+    },
+    git_files = {
+      hidden = true,
+      previewer = false
     }
   },
   extensions = {},
