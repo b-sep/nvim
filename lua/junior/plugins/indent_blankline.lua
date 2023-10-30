56b54cb3fc9a8eb2777c8d8b4561f956e229ca13
Júnior <b.sep@live.com>
2023-10-29 21:00:52 -0300

    fix ibl

--- lua/junior/plugins/indent_blankline.lua
+++ lua/junior/plugins/indent_blankline.lua
@@ -1,12 +1,14 @@
-local status_ok, indent_blankline = pcall(require, 'indent_blankline')
+local status_ok, indent_blankline = pcall(require, 'ibl')
 if not status_ok then
   return
 end
 
-indent_blankline.setup {
-  char = "▏",
-  char_blankline = "▏",
-  show_current_context = true,
-  show_trailing_blankline_indent = false,
-  use_treesitter = true,
-}
+indent_blankline.setup()
+
+-- indent_blankline.setup {
+--   char = "▏",
+--   char_blankline = "▏",
+--   show_current_context = true,
+--   show_trailing_blankline_indent = false,
+--   use_treesitter = true,
+-- }
