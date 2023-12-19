ea88a61d9ce77120512c678a70a6131ba82526b2
Júnior <b.sep@live.com>
2023-12-19 20:31:21 -0300

    fix settings to ufo

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -39,7 +39,7 @@ return {
     fold_virt_text_handler = handler_virt_text
   },
   init = function()
-    vim.o.foldcolumn = '1'
+    vim.o.foldcolumn = '0'
     vim.o.foldlevel = 99
     vim.o.foldlevelstart = 99
     vim.o.foldenable = true
