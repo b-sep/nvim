f66254542a47cc8e462ecf8ae86651c53e929394
Júnior <b.sep@live.com>
2026-04-08 21:12:16 -0300

    config nvim-treesitter to match nvim 0.12

--- lua/plugins/nvim-treesitter-endwise.lua
+++ lua/plugins/nvim-treesitter-endwise.lua
@@ -1,11 +1,4 @@
 return {
   'RRethy/nvim-treesitter-endwise',
-  ft = "ruby",
-  config = function()
-    require('nvim-treesitter.configs').setup {
-      endwise = {
-        enable = true,
-      },
-    }
-  end
+  ft = "ruby"
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,27 +3,36 @@ return {
   branch = 'main',
   lazy = false,
   build = ':TSUpdate',
-  config = function ()
-    local configs = require('nvim-treesitter.configs')
+  init = function()
+    ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'ruby', 'typescript', 'python', 'elixir', 'javascript', 'html', 'css', 'erlang', 'sql' }
+    local alreadyInstalled = require('nvim-treesitter.config').get_installed()
+    local parsersToInstall = vim.iter(ensure_installed)
+      :filter(function(parser)
+        return not vim.tbl_contains(alreadyInstalled, parser)
+      end)
+      :totable()
 
-    configs.setup({
-      auto_install = false,
-      ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'ruby', 'typescript', 'python', 'elixir', 'heex', 'javascript', 'html', 'css', 'erlang', 'sql' },
-      incremental_selection = { enable = false },
-      indent = { enable = true, disable = { 'ruby', 'css' } },
-      highlight = {
-        additional_vim_regex_highlighting = true,
-        disable = function(lang, buf) -- disable tree-sitter highlight in big files
-          local max_filesize = 100 * 1024
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+    require('nvim-treesitter').install(parsersToInstall)
 
-          if ok and stats and stats.size > max_filesize then
-            return true
-          end
-        end,
-        enable = true,
-      },
-      sync_install = false,
+    local indent_disabled = { ruby = true, css = true }
+
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function(args)
+        if not indent_disabled[vim.bo[args.buf].filetype] then
+          -- Enable treesitter-based indentation
+          -- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+
+        -- local max_filesize = 100 * 1024
+        -- local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(args.buf))
+        -- if ok and stats and stats.size > max_filesize then
+        --   return  -- skip treesitter for big files
+        -- end
+
+        -- Enable treesitter highlighting and disable regex syntax
+        pcall(vim.treesitter.start)
+      end,
     })
   end
 }
